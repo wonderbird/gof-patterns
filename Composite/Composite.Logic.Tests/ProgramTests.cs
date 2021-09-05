677cd5974ee059058b87df8c2eabd9eb10d2151f
Stefan Boos <kontakt@boos.systems>
2021-09-05 15:05:36 +0200

    refactor(Composite): simplify tests and remove duplication

--- Composite/Composite.Logic.Tests/ProgramTests.cs
+++ Composite/Composite.Logic.Tests/ProgramTests.cs
@@ -11,6 +11,7 @@ namespace Composite.Logic.Tests
 
         private readonly Mock<IReader> _inputMock;
         private readonly Mock<IWriter> _outputMock;
+        private int _expectedReadCalls;
 
         public ProgramTests()
         {
@@ -25,60 +26,47 @@ namespace Composite.Logic.Tests
         [InlineData("rect 1 2 10 20", "(1, 2) (11, 22)")]
         [InlineData("rect 0 0 0 0", "(0, 0) (0, 0)")]
         [InlineData("rect 90 90 10 10", "(90, 90) (100, 100)")]
-        public void Main_SingleRectangle_PrintsBoundingBox(string input, string expected)
+        public void Main_SingleRectangle_PrintsBoundingBox(string input, string expectedBoundingBox)
         {
-            _inputMock.SetupSequence(x => x.Read())
-                .Returns(input)
-                .Returns("");
-
-            Program.Main(null);
-
-            var expectedReadCalls = 2;
-            _inputMock.Verify(x => x.Read(), Times.Exactly(expectedReadCalls));
-            _outputMock.Verify(x => x.Write(expected));
-            _outputMock.Verify(x => x.Write(It.IsAny<string>()), Times.Exactly(NumberOfDocumentationMessages + 1));
+            GivenUserInput_WhenProgramRuns_ThenBoundingBoxIsPrintedAndAllInputIsConsumed(new[] { input },
+                expectedBoundingBox);
         }
 
         [Theory]
         [InlineData("circle 1 2 5", "(-4, -3) (6, 7)")]
-        public void Main_SingleCircle_PrintsBoundingBox(string input, string expected)
+        public void Main_SingleCircle_PrintsBoundingBox(string input, string expectedBoundingBox)
         {
-            _inputMock.SetupSequence(x => x.Read())
-                .Returns(input)
-                .Returns("");
-
-            Program.Main(null);
-
-            var expectedReadCalls = 2;
-            _inputMock.Verify(x => x.Read(), Times.Exactly(expectedReadCalls));
-            _outputMock.Verify(x => x.Write(expected));
-            _outputMock.Verify(x => x.Write(It.IsAny<string>()), Times.Exactly(NumberOfDocumentationMessages + 1));
+            GivenUserInput_WhenProgramRuns_ThenBoundingBoxIsPrintedAndAllInputIsConsumed(new[] { input },
+                expectedBoundingBox);
         }
 
         [Theory]
         [InlineData(new[] { "rect 5 5 5 10", "rect 8 8 1 10" }, "(5, 5) (10, 18)")]
         [InlineData(new[] { "rect 20 20 100 100", "circle 170 170 50" }, "(20, 20) (220, 220)")]
-        public void Main_MultipleShapes_PrintsCombinedBoundingBox(string[] input, string expected)
+        public void Main_MultipleShapes_PrintsCombinedBoundingBox(string[] input, string expectedBoundingBox)
         {
-            var readInvocation = _inputMock.SetupSequence(x => x.Read());
-            foreach (var current in input)
-            {
-                readInvocation.Returns(current);
-            }
-
-            readInvocation.Returns("");
-
-            Program.Main(null);
-
-            var expectedReadCalls = input.Length + 1;
-            _inputMock.Verify(x => x.Read(), Times.Exactly(expectedReadCalls));
-            _outputMock.Verify(x => x.Write(expected));
-            _outputMock.Verify(x => x.Write(It.IsAny<string>()), Times.Exactly(NumberOfDocumentationMessages + 1));
+            GivenUserInput_WhenProgramRuns_ThenBoundingBoxIsPrintedAndAllInputIsConsumed(input, expectedBoundingBox);
         }
 
         [Theory]
         [InlineData(new[] { "scale group 1.0", "circle 170 170 50", "end group" }, "(120, 120) (220, 220)")]
-        public void Main_ScaleGroup_PrintsScaledBoundingBox(string[] input, string expected)
+        //[InlineData(new[] { "scale group 0.1", "circle 170 170 50", "end group" }, "(120, 120) (130, 130)")]
+        public void Main_ScaleGroup_PrintsScaledBoundingBox(string[] input, string expectedBoundingBox)
+        {
+            GivenUserInput_WhenProgramRuns_ThenBoundingBoxIsPrintedAndAllInputIsConsumed(input, expectedBoundingBox);
+        }
+
+        private void GivenUserInput_WhenProgramRuns_ThenBoundingBoxIsPrintedAndAllInputIsConsumed(string[] input,
+            string expectedBoundingBox)
+        {
+            SetupUserInput(input);
+
+            Program.Main(null);
+
+            AssertBoundingBoxIsPrintedAndAllInputIsConsumed(expectedBoundingBox);
+        }
+
+        private void SetupUserInput(string[] input)
         {
             var readInvocation = _inputMock.SetupSequence(x => x.Read());
             foreach (var current in input)
@@ -88,11 +76,13 @@ namespace Composite.Logic.Tests
 
             readInvocation.Returns("");
 
-            Program.Main(null);
+            _expectedReadCalls = input.Length + 1;
+        }
 
-            var expectedReadCalls = input.Length + 1;
-            _inputMock.Verify(x => x.Read(), Times.Exactly(expectedReadCalls));
-            _outputMock.Verify(x => x.Write(expected));
+        private void AssertBoundingBoxIsPrintedAndAllInputIsConsumed(string expectedBoundingBox)
+        {
+            _inputMock.Verify(x => x.Read(), Times.Exactly(_expectedReadCalls));
+            _outputMock.Verify(x => x.Write(expectedBoundingBox));
             _outputMock.Verify(x => x.Write(It.IsAny<string>()), Times.Exactly(NumberOfDocumentationMessages + 1));
         }
     }
