e15277a1211719b288b1d3dfc9ead17a83ccf933
Stefan Boos <kontakt@boos.systems>
2021-10-30 23:24:11 +0200

    refactor(repository test): move database creation to setup

--- Repository/TestSqliteExerciseRepository.pas
+++ Repository/TestSqliteExerciseRepository.pas
@@ -3,28 +3,32 @@ unit TestSqliteExerciseRepository;
 interface
 
 uses
-  DUnitX.TestFramework;
+  DUnitX.TestFramework, FireDAC.Comp.Client;
 
 type
 
   [TestFixture]
   TestTSqliteExerciseRepository = class
+  private
+    FConnection: TFDConnection;
   public
     [Test]
     [Ignore('Not implemented yet.')]
     procedure Add;
-
     [Test]
     procedure Find_EmptyRepository_ReturnsEmptyCollection;
     [Test]
     procedure Find_2EntriesInDatabase_ReturnsCollectionWith2Entries;
+    procedure RecreateEmptyDatabase;
+    [Setup]
+    procedure Setup;
 
   end;
 
 implementation
 
 uses
-  FireDAC.Comp.Client, FireDAC.Phys.SQLiteDef, SqliteDatabaseConfiguration,
+  FireDAC.Phys.SQLiteDef, SqliteDatabaseConfiguration,
   Spring.Collections, ExerciseRepository, Exercise, SqliteExerciseRepository,
   System.SysUtils;
 
@@ -55,24 +59,32 @@ end;
 procedure
     TestTSqliteExerciseRepository.Find_2EntriesInDatabase_ReturnsCollectionWith2Entries;
 var
-  Connection: TFDConnection;
   Repository: IExerciseRepository;
   Rows: IEnumerable<TExercise>;
 begin
-  DeleteFile(TSqliteDatabaseConfiguration.DatabaseFileName);
-
-  Connection := TFDConnection.Create(nil);
-  Connection.ConnectionDefName := TSqliteDatabaseConfiguration.ConnectionDefinitionName;
-  Connection.Connected := True;
-
-  Connection.ExecSQL('CREATE TABLE IF NOT EXISTS exercises (id INTEGER PRIMARY KEY AUTOINCREMENT, start DATETIME)');
-  Connection.ExecSQL('INSERT INTO exercises VALUES (NULL, ''2021-10-08T07:00:00.000Z'')');
-  Connection.ExecSQL('INSERT INTO exercises VALUES (NULL, ''2021-10-09T07:00:00.000Z'')');
+  FConnection.ExecSQL('INSERT INTO exercises VALUES (NULL, ''2021-10-08T07:00:00.000Z'')');
+  FConnection.ExecSQL('INSERT INTO exercises VALUES (NULL, ''2021-10-09T07:00:00.000Z'')');
 
   Repository := TSqliteExerciseRepository.Create();
   Rows := Repository.Find;
 
   Assert.AreEqual(2, Rows.Count, 'unexpected number of rows');
+end;
+
+procedure TestTSqliteExerciseRepository.RecreateEmptyDatabase;
+begin
+  DeleteFile(TSqliteDatabaseConfiguration.DatabaseFileName);
+
+  FConnection := TFDConnection.Create(nil);
+  FConnection.ConnectionDefName := TSqliteDatabaseConfiguration.ConnectionDefinitionName;
+  FConnection.Connected := True;
+
+  FConnection.ExecSQL('CREATE TABLE IF NOT EXISTS exercises (id INTEGER PRIMARY KEY AUTOINCREMENT, start DATETIME)');
+end;
+
+procedure TestTSqliteExerciseRepository.Setup;
+begin
+  RecreateEmptyDatabase;
 end;
 
 initialization
