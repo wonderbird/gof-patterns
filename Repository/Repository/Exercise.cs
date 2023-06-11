bbc858bd49f3f58b784f07586c3e3dacb72d3727
Stefan Boos <kontakt@boos.systems>
2023-06-11 12:04:57 +0200

    refactor: convert Exercise record to entity

--- Repository/Repository/Exercise.cs
+++ Repository/Repository/Exercise.cs
@@ -1,3 +1,62 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Repository;
 
-public record Exercise(DateTime Start);
+public sealed class Exercise : IEquatable<Exercise>
+{
+    public int Id { get; set; }
+
+    public DateTime Start { get; set; }
+
+    public Exercise(DateTime start) => Start = start;
+
+    #region Equality Members
+
+    public bool Equals(Exercise? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return Start.Equals(other.Start);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != this.GetType())
+        {
+            return false;
+        }
+
+        return Equals((Exercise) obj);
+    }
+
+    [SuppressMessage(
+        "Sonar Code Smell",
+        "S3877:Exceptions should not be thrown from unexpected methods",
+        Justification = "The Exercise entity is not a value object. Thus it should not be used with operations and data types relying on GetHashCode()."
+    )]
+    public override int GetHashCode() => throw new NotSupportedException("The Exercise entity is not a value object. Thus it should not be used with operations and data types relying on GetHashCode().");
+
+    public static bool operator ==(Exercise? left, Exercise? right) => Equals(left, right);
+
+    public static bool operator !=(Exercise? left, Exercise? right) => !Equals(left, right);
+
+    #endregion
+}
